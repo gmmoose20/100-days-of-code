1679d90d90a69b70cb0a9fb0d08d9133789053a3
gms <gmmoose17@gmail.com>
2021-01-29 18:44:23 +0800

    :white_check_mark: done

---
+++ Day11_VueJSisAwesome/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Header title="VueJS is AWESOME"></Header>
+    <Profile :user="user"></Profile>
+  </div>
+</template>
+
+<script>
+import  Header  from "./components/Header";
+import  Profile  from "./components/Profile";
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Profile
+  },
+
+  data: function () {
+    return {
+      user: {
+        name: 'Gm Awesome',
+        image: require('./assets/spiderman.jpg')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  background: #f3f3f3;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
+}
+</style>
---
+++ Day11_VueJSisAwesome/src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="../assets/logo.png" alt="VueJS Logo">
+    </div>
+
+    <div class="title">
+      <h1>{{ title }}</h1>
+    </div>
+
+    <div class="filler"></div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    width: 100%;
+    height: 60px;
+    background: #171717;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .logo {
+    flex: 1;
+  }
+
+  .logo img {
+    height: 35px;
+  }
+  
+  .title {
+    flex: 3;
+  }
+
+  .title h1 {
+    color: #f3f3f3;
+    font-size: 28px;
+    line-height: 60px;
+    font-weight: 300;
+  }
+
+  .filler {
+    flex: 1;
+  }
+</style>
