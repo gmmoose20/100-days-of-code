b22357a316cf95de2c54248b05504baef75f5166
gms <gmmoose17@gmail.com>
2021-02-20 17:47:26 +0800

    ✔ to be continued

---
+++ Day30_Blog/blog/src/components/Posts.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="posts">
+    <button class="load">
+      See More
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'posts'
+}
+</script>
+
+<style lang="scss" scoped>
+.posts {
+  width: 100%;
+  max-width: 1280px;
+  
+  margin: 50px auto;
+
+  .load {
+    display: block;
+    width: 100%;
+    max-width: 250px;
+    height: 50px;
+
+    margin: 0 auto;
+
+    outline: none;
+    border: none;
+    border-radius: 15px;
+    appearance: none;
+
+    background-color: #469dca;
+    box-shadow: 3px 3px 8px 0 rgba(0, 0, 0, 0.2);
+    
+    color: #171717;
+    font-family: 'Gaegu', cursive;
+    font-size: 28px;
+
+    padding-top: 7px;
+    box-sizing: border-box;
+  }
+}
+</style>
