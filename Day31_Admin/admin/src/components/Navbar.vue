c457d8751659385d6c256ed42fc58d95aefc47c9
gms <gmmoose17@gmail.com>
2021-02-23 00:07:36 +0800

    ✔ to be continued

---
+++ Day31_Admin/admin/src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <v-app-bar dark>
+      <v-app-bar-nav-icon />
+      <v-toolbar-title class="text-uppercase cyan--text">
+        Gm Awesome's <span class="font-weight-light">Mind</span>
+      </v-toolbar-title>
+    </v-app-bar> 
+  </nav>
+</template>
