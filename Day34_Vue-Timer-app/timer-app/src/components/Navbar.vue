29c7dc033c5b47c6d5cbfba7bbeaed16f7f3aa76
gms <gmmoose17@gmail.com>
2021-03-06 21:01:04 +0800

    ✔ to be continued

--- Day34_Vue-Timer-app/timer-app/src/components/Navbar.vue
+++ Day34_Vue-Timer-app/timer-app/src/components/Navbar.vue
@@ -1,11 +1,27 @@
 <template>
   <nav>
     <v-app-bar app dark class="secondary">
-      <v-app-bar-nav-icon />
+      <v-app-bar-nav-icon @click="isDrawerOpen = !isDrawerOpen"/>
         <v-app-bar-title class="headline text-uppercase">
           <span class="font-weight-black primary--text">Gm's</span>
           <span class="font-weight-light">Awesome Timer</span>
         </v-app-bar-title>
     </v-app-bar>
+    <v-navigation-drawer app dark v-model="isDrawerOpen">
+      <v-app-bar class="secondary">
+        <v-app-bar-title class="title">Laps</v-app-bar-title>
+      </v-app-bar>
+    </v-navigation-drawer>
   </nav>
 </template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => {
+    return {
+      isDrawerOpen: false
+    }
+  }
+}
+</script>
